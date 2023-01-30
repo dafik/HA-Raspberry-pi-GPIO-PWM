1ff80c2e4e3d9a830ba979e3c352dca1747df732
Zbyszek Wieczorek <zbigniew.wieczorek@envelo.pl>
2023-01-30 19:50:19 +0100

    debug

--- custom_components/ha-rpi_gpio_pwm/light.py
+++ custom_components/ha-rpi_gpio_pwm/light.py
@@ -2,12 +2,13 @@
 from __future__ import annotations
 
 import logging
+import threading
+import time
 
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 from gpiozero import PWMLED
 from gpiozero.pins.pigpio import PiGPIOFactory
-
-import voluptuous as vol
-
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     ATTR_TRANSITION,
@@ -24,13 +25,11 @@ from homeassistant.const import (
     CONF_UNIQUE_ID
 )
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from singleton.singleton import Singleton
 
-from transition import Transition
-from transition_manager import TransitionManager
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -128,7 +127,7 @@ class PwmSimpleLed(LightEntity, RestoreEntity):
     def turn_on(self, **kwargs):
         _LOGGER.info("TURN ON: " + self.name + " args: " + str(locals()))
         """Turn on a led."""
-        self._cancel_active_transition();
+        self._cancel_active_transition()
         if ATTR_BRIGHTNESS in kwargs and ATTR_TRANSITION in kwargs:
             self._transition(kwargs[ATTR_TRANSITION], 0, _from_hass_brightness(kwargs[ATTR_BRIGHTNESS]))
 
@@ -140,7 +139,7 @@ class PwmSimpleLed(LightEntity, RestoreEntity):
 
     def turn_off(self, **kwargs):
         _LOGGER.info("TURN ON: " + self.name + " args: " + str(locals()))
-        self._cancel_active_transition();
+        self._cancel_active_transition()
         """Turn off a LED."""
         if self.is_on:
             self._led.off()
@@ -170,6 +169,159 @@ class PwmSimpleLed(LightEntity, RestoreEntity):
         if self._active_transition:
             self._active_transition.cancel()
             self._active_transition = None
+
+
+class Transition:
+    """Represents a transition of a LED."""
+    _led: PwmSimpleLed
+
+    def __init__(self, led: PwmSimpleLed, duration, from_brightness, to_brightness):
+        """
+        Initialize the transition.
+        :param led: The led to control.
+        :param duration: The duration.
+        :param from_brightness: The source state of the led.
+        :param to_brightness: The target state of the led.
+        """
+        self._led = led
+        self._duration = duration
+        self._from_brightness = from_brightness
+        self._to_brightness = to_brightness
+
+        self._cancelled = False
+        self._finish_event = threading.Event()
+        self._start_time = time.perf_counter()
+
+    @property
+    def duration(self):
+        """
+        Duration property.
+        :return: The duration of the transition.
+        """
+        return self._duration
+
+    @property
+    def progress(self):
+        """
+        Progress property.
+        :return: The progress of the transition (0.0-1.0).
+        """
+        if self._duration == 0:
+            return 1
+
+        run_time = time.perf_counter() - self._start_time
+        return max(0, min(1, run_time / self._duration))
+
+    @property
+    def finished(self):
+        """
+        Finshed property.
+        :return: True, if transition has finished. False otherwise.
+        """
+        return self._finish_event.is_set()
+
+    @property
+    def cancelled(self):
+        """
+        Cancelled property.
+        :return: True, if transition was cancelled. False otherwise.
+        """
+        return self._cancelled
+
+    def step(self):
+        """Apply the current stage of the transition based on current time."""
+        if self.cancelled or self.finished:
+            return
+
+        if self.progress == 1:
+            self._finish()
+            return
+
+        state = {}
+        src_is_on = self._from_brightness == 0
+        dest_is_on = self._to_brightness == 0
+
+        src_brightness = self._from_brightness
+        dest_brightness = self._to_brightness
+        if src_is_on is False:
+            if dest_brightness is None:
+                dest_brightness = src_brightness
+            src_brightness = 0
+        if dest_is_on is False:
+            dest_brightness = 0
+        if src_brightness is not None and dest_brightness is not None:
+            self._led.set_brightness(self._interpolate(
+                src_brightness,
+                dest_brightness,
+            ))
+
+    def _interpolate(self, start, end):
+        """
+        Interpolate a value from start to end at the current progress.
+        :param start: The start value.
+        :param end: The end value.
+        :return: The interpolated value at the current progress.
+        """
+        diff = end - start
+        return start + self.progress * diff
+
+    def _finish(self):
+        """Complete transition and mark it as finished."""
+        state = self._to_brightness
+        self._led.set_brightness(state)
+
+        self._finish_event.set()
+
+    def wait(self, timeout=None):
+        """
+        Wait for transition to be finished.
+        :param timeout: Timeout of the operation in seconds.
+        """
+        self._finish_event.wait(timeout=timeout)
+
+    def cancel(self):
+        """Cancel the transition."""
+        self._cancelled = True
+        self._finish_event.set()
+
+
+class TransitionManager(object, metaclass=Singleton):
+    """Represents a manager that executes transitions in a separate thread."""
+    _thread: threading.Thread
+    _transitions: list[Transition]
+
+    STEP_TIME = 0.001
+
+    def __init__(self):
+        """Initialize the manager."""
+        self._thread = None
+        self._transitions = []
+
+    def execute(self, transition: Transition):
+        """
+        Queue a transition for execution.
+        :param transition: The transition
+        :return: The started transition.
+        """
+        self._transitions.append(transition)
+        if self._thread is None or not self._thread.is_alive():
+            self._thread = threading.Thread(
+                target=self._transition_loop,
+                daemon=True,
+            )
+            self._thread.start()
+
+        return transition
+
+    def _transition_loop(self):
+        """Execute all queued transitions step by step."""
+        while self._transitions:
+            for transition in self._transitions:
+                transition.step()
+                if transition.finished:
+                    self._transitions.remove(transition)
+
+            time.sleep(self.STEP_TIME)
 
 
 def _from_hass_brightness(brightness):
